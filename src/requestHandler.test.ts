2f0ceb8a8c0c2f49b07718baaea05484c8e8c272
Adam Coddington <me@adamcoddington.net>
2022-01-30 14:34:03 -0800

    Fixing some tests; adding a few more.

--- src/requestHandler.test.ts
+++ src/requestHandler.test.ts
@@ -4,53 +4,94 @@ import request from "supertest";
 
 import RequestHandler from "./requestHandler";
 import { LocalRestApiSettings } from "./types";
-
-const API_KEY = "my api key";
-
-let settings: LocalRestApiSettings;
-let app: App;
-let handler: RequestHandler;
-let server: http.Server;
-
-beforeEach(() => {
-  settings = getMockSettings();
-  app = new App();
-  handler = new RequestHandler(app, settings);
-  server = http.createServer(handler.api);
-});
-
-afterEach(() => {
-  server.close();
-});
-
-function getMockSettings(): LocalRestApiSettings {
-  return {
-    apiKey: API_KEY,
-    crypto: {
-      cert: "cert",
-      privateKey: "privateKey",
-      publicKey: "publicKey",
-    },
-    port: 1,
-  };
-}
+import { CERT_NAME } from "./constants";
 
 describe("requestHandler", () => {
+  const API_KEY = "my api key";
+
+  let settings: LocalRestApiSettings;
+  let app: App;
+  let handler: RequestHandler;
+  let server: http.Server;
+
+  beforeEach(() => {
+    settings = getMockSettings();
+    app = new App();
+    handler = new RequestHandler(app, settings);
+    handler.setupRouter();
+    server = http.createServer(handler.api);
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
+  function getMockSettings(): LocalRestApiSettings {
+    return {
+      apiKey: API_KEY,
+      crypto: {
+        cert: "cert",
+        privateKey: "privateKey",
+        publicKey: "publicKey",
+      },
+      port: 1,
+    };
+  }
   describe("requestIsAuthenticated", () => {
-    test("missing header", () => {
-      request(server).get("/vault/").expect(401);
+    const arbitraryAuthenticatedRoute = "/vault/";
+
+    test("missing header", async () => {
+      await request(server).get(arbitraryAuthenticatedRoute).expect(401);
     });
-    test("incorrect header", () => {
-      request(server)
-        .get("/vault/")
+    test("incorrect header", async () => {
+      await request(server)
+        .get(arbitraryAuthenticatedRoute)
         .set("Authorization", "Bearer of good tidings")
         .expect(401);
     });
-    test("correct header", () => {
-      request(server)
-        .get("/vault/")
+    test("correct header", async () => {
+      await request(server)
+        .get(arbitraryAuthenticatedRoute)
         .set("Authorization", `Bearer ${API_KEY}`)
         .expect(200);
+    });
+  });
+
+  describe("root", () => {
+    test("withhout auth", async () => {
+      const result = await request(server).get("/").expect(200);
+
+      expect(result.body.status).toEqual("OK");
+      expect(result.body.authenticated).toBeFalsy();
+    });
+
+    test("with auth", async () => {
+      const result = await request(server)
+        .get("/")
+        .set("Authorization", `Bearer ${API_KEY}`)
+        .expect(200);
+
+      expect(result.body.status).toEqual("OK");
+      expect(result.body.authenticated).toBeTruthy();
+    });
+  });
+
+  describe("certificateGet", () => {
+    const certPath = `/${CERT_NAME}`;
+
+    test("withhout auth", async () => {
+      const result = await request(server).get(certPath).expect(200);
+
+      expect(result.body.toString()).toEqual(settings.crypto.cert);
+    });
+
+    test("with auth", async () => {
+      const result = await request(server)
+        .get(certPath)
+        .set("Authorization", `Bearer ${API_KEY}`)
+        .expect(200);
+
+      expect(result.body.toString()).toEqual(settings.crypto.cert);
     });
   });
 });
