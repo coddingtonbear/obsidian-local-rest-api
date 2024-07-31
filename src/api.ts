be25c7787a09b0343a5c6255bec1de704c31b7c3
Adam Coddington <me@adamcoddington.net>
2024-07-30 19:13:30 -0500

    Adding some utility functions that might make it easier to use this API.

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,8 @@
-import { IRoute } from "express";
+import { IRoute, Request } from "express";
 import RequestHandler from "./requestHandler";
 import { PluginManifest } from "mocks/obsidian";
+import { TFile } from "obsidian";
+import { FileMetadataObject } from "./types";
 
 export default class LocalRestApiPublicApi {
   private requestHandler: RequestHandler;
@@ -11,7 +13,18 @@ export default class LocalRestApiPublicApi {
     this.requestHandler = requestHandler;
   }
 
+  /** Adds a route to the request handler. */
   public addRoute(path: string): IRoute {
     return this.requestHandler.api.route(path);
+  }
+
+  /** Is the provided request authenticated? */
+  public requestIsAuthenticated(request: Request): boolean {
+    return this.requestHandler.requestIsAuthenticated(request);
+  }
+
+  /** Returns a `FileMetadataObject` for a provided obsidian file */
+  public getFileMetadataObject(file: TFile): Promise<FileMetadataObject> {
+    return this.requestHandler.getFileMetadataObject(file);
   }
 }
