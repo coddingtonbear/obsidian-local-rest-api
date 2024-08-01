cdbc39f3611a6a7c5d02ff4670c9a1f58cb9f519
Adam Coddington <me@adamcoddington.net>
2024-07-31 22:15:28 -0500

    Adding fully-working registration/unregistration of new routes provided by a plugin.

--- main.d.ts
+++ main.d.ts
@@ -1,15 +1,12 @@
 import { App, PluginManifest } from "obsidian";
 
-import { IRoute, Request } from "express";
+import { IRoute } from "express";
 
 declare class LocalRestApiPublicApi {
-  public manifest: PluginManifest;
-
   /** Adds a route to the request handler. */
   public addRoute(path: string): IRoute;
 
-  /** Is the provided request authenticated? */
-  public requestIsAuthenticated(request: Request): boolean;
+  public unregister(): void;
 }
 
 export function getAPI(
