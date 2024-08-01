ac0129a7d59a7036b18152e97cc2cc3dafe49d7a
Adam Coddington <me@adamcoddington.net>
2024-08-01 07:45:20 -0500

    Raise error if endpoints are added to unregistered API extensions.

--- src/api.ts
+++ src/api.ts
@@ -3,18 +3,26 @@ import express from "express";
 export default class LocalRestApiPublicApi {
   private router: express.Router;
   private unregisterHandler: () => void;
+  private unregistered = false;
 
   constructor(router: express.Router, unregister: () => void) {
     this.router = router;
     this.unregisterHandler = unregister;
+    this.unregistered = false;
   }
 
   /** Adds a route to the request handler. */
   public addRoute(path: string): express.IRoute {
+    if (this.unregistered) {
+      throw new Error(
+        "Routes cannot be added after API extension has been unregistered."
+      );
+    }
     return this.router.route(path);
   }
 
   public unregister(): void {
     this.unregisterHandler();
+    this.unregistered = true;
   }
 }
